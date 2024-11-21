644a6675b53f6e7081304eb00c8ce4ffde70854f
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-21 12:17:06 +0200

    Create interface for 'Task'

---
+++ interface.ts
@@ -0,0 +1,10 @@
+
+export default interface Task {
+    id: number;
+    title: string;
+    description: string;
+    completed: boolean;
+    urgency: ;
+    dateCreated: Date;
+    deadlineDate: Date;
+}
