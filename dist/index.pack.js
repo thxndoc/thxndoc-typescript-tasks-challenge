cf4f12a3c311ed8786f7600e146e09afee48ae79
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-11-21 12:13:27 +0200

    Setup project

---
+++ dist/index.pack.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.querySelector("#reviews"),console.log([{name:"Sheia",stars:5,loyaltyUser:!0,date:"01-04-2021"},{name:"Andrzej",stars:3,loyaltyUser:!1,date:"28-03-2021"},{name:"Omar",stars:4,loyaltyUser:!0,date:"27-03-2021"}])})();
